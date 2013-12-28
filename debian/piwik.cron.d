b2aabac36269c20c80bbcd6c23cdb492e4096a42
Aurelien Requiem <aurelien@requiem.fr>
2013-12-28 12:34:31 +1100

    piwik (2.0.2-2) stable; urgency=high
      * fixed postinst when calling invoke-rc.d
      * postinst now mention the cron in /etc/cron.d/piwik
    
     -- Aurelien Requiem <aurelien@requiem.fr>  Sat, 28 Dec 2013 12:24:43 +1100

--- debian/piwik.cron.d
+++ debian/piwik.cron.d
@@ -1,9 +1,9 @@
 # set and uncomment the cron as you like
 # .---------------- minute (0 - 59)
 # | .-------------- hour (0 - 23)
-# | |   .---------- day of month (1 - 31)
-# | |   | .-------- month (1 - 12) OR jan,feb,mar,apr ...
-# | |   | | .------ day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
-# | |   | | |
-# * *   * * *   user-name       command to be executed
-#5 *	* * *	www-data        [ -x /usr/share/piwik/misc/cron/archive.sh ] && /usr/share/piwik/misc/cron/archive.sh >/dev/null 2>&1
+# | |	.---------- day of month (1 - 31)
+# | |	| .-------- month (1 - 12) OR jan,feb,mar,apr ...
+# | |	| | .------ day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
+# | |	| | |
+# * *	* * *	user-name       command to be executed
+# 5 *	* * *	www-data        [ -x /usr/share/piwik/misc/cron/archive.sh ] && /usr/share/piwik/misc/cron/archive.sh >/dev/null 2>&1

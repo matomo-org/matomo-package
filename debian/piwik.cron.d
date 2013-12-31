1eea2789cdc2de2f3edfd9887d5d03510b9fb575
Aurelien Requiem <aurelien@requiem.fr>
2013-12-31 15:01:17 +1100

    fixed the way piwik cron is executed

--- debian/piwik.cron.d
+++ debian/piwik.cron.d
@@ -6,4 +6,4 @@
 # | |	| | .------ day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
 # | |	| | |
 # * *	* * *	user-name       command to be executed
-# 5 *	* * *	www-data        [ -x /usr/share/piwik/misc/cron/archive.sh ] && /usr/share/piwik/misc/cron/archive.sh >/dev/null 2>&1
+# 5 *	* * *	www-data        [ -e /usr/share/piwik/misc/cron/archive.php ] && [ -x /usr/bin/php ] && /usr/bin/php /usr/share/piwik/misc/cron/archive.php -- "url=http://example.org/piwik/" >/dev/null 2>&1

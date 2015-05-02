1d72c42d4e4d4e865c8401af44d7e554b9353518
Aurélien Requiem <aurelien@requiem.fr>
2015-05-02 12:57:58 +1000

    when a release is made, a tag is also added and signed

--- debian/scripts/githelp.sh
+++ debian/scripts/githelp.sh
@@ -13,6 +13,9 @@ then
 		echo "One or more files needs to be committed"
 		exit 1
 	else
+		RELEASE=$(head -n 1 debian/changelog | sed 's/[()]//g' | awk '{print $2}')
 		git commit -S -m "$(head -n 1 debian/changelog)" debian/changelog
+		git tag --sign -m "release ${RELEASE}" ${RELEASE}
+
 	fi
 fi

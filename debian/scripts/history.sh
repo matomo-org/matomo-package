bbd874a48463a0d55804f1b79aca27497dcd169e
Aurélien Requiem <aurelien@requiem.fr>
2017-01-14 10:02:39 +1100

    ensure we only get the release specific url

--- debian/scripts/history.sh
+++ debian/scripts/history.sh
@@ -30,7 +30,7 @@ then
 	exit 1
 fi
 
-CHANGELOG_URL=$(wget -O - -q 'http://piwik.org/changelog/' | grep "Piwik $1" | sed 's/.*<a href=\([^>]*\).*/\1/' | sed -e 's/"//g' -e "s/'//g" | grep ^http)
+CHANGELOG_URL=$(wget -O - -q 'http://piwik.org/changelog/' | grep "Piwik $1" | sed 's/.*<a href=\([^>]*\).*/\1/' | sed -e 's/"//g' -e "s/'//g" | grep ^http | grep "$1/")
 
 if [ -z "$(echo $CHANGELOG_URL | grep -i http)" ]
 then

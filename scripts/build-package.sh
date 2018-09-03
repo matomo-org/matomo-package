3141bb78367b46297420ef602ce44be79d2e8753
Lukas Winkler <git@lw1.at>
2018-09-03 19:16:52 +0200

    better code style

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -324,7 +324,7 @@ for F in $FLAVOUR; do
 		BUILDING_LATEST_MAJOR_VERSION_STABLE_OR_BETA=0
 	fi
 
-	if [ ! "$(echo "$VERSION" | grep -E 'rc|b|a|alpha|beta|dev' -i | wc -l)" -eq 1 ]
+	if ! echo "$VERSION" | grep -E 'rc|b|a|alpha|beta|dev' -i
 	then
 		if curl --output /dev/null --silent --head --fail "https://builds.matomo.org/$F-$VERSION.zip"
 		then

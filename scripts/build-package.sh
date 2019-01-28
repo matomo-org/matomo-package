4311e8ab87b5cc8d3aaedee82b41afce6460a44f
diosmosis <diosmosis@users.noreply.github.com>
2019-01-27 22:02:00 -0800

    Adding BUILD_ONLY option, remove some git commands to favor --depth=1 option for fastest clone, fix checkEnv conditions and make cp/find specified through variables so they can be changed for os x.

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -41,6 +41,9 @@ REMOTE_CMD_WWW="ssh -C piwik@${REMOTE_SERVER}"
 API_PATH="/home/piwik-api/www/api.piwik.org/"
 WWW_PATH="/home/piwik/www/"
 
+# Change these to gcp/gfind on mac (get from the appropriate homebrew packages)
+CP=cp
+FIND=find
 
 # Setting umask so it works for most users, see https://github.com/matomo-org/matomo/issues/3869
 UMASK=$(umask)
@@ -53,6 +56,8 @@ CURRENT_DIR="$(pwd)"
 WORK_DIR="$(mktemp -d)"
 cd "$WORK_DIR"
 
+echo "Working directory is '$WORK_DIR'..."
+
 # this is where our Matomo is going to be built
 BUILD_DIR=$WORK_DIR/archives/
 
@@ -60,10 +65,11 @@ trap "script_cleanup" EXIT
 
 function Usage() {
 	echo -e "ERROR: This command is missing one or more option. See help below."
-	echo -e "$0 version [flavour] [cache]"
+	echo -e "$0 version [flavour] [cache] [build-only]"
 	echo -e "\t* version: Package version under which you want the archive to be published."
 	echo -e "\t* flavour: Base name of your archive. Can either be 'matomo' or 'piwik'. If unspecified, both archives are generated."
 	echo -e "\t* cache: Set to 'true' if you want to use a repository-cache in $CACHE_DIRECTORY."
+	echo -e "\t* build-only: Set to 'true' if you want to build the package without uploading to the builds server. This will skip the version check as well."
 	# exit with code 1 to indicate an error.
 	exit 1
 }
@@ -71,37 +77,42 @@ function Usage() {
 
 # check local environment for all required apps/tools
 function checkEnv() {
-	if [ ! -x "/usr/bin/curl" -o ! -x "$(which curl)" ]
+	if [ ! -x "/usr/bin/curl" -a ! -x "$(which curl)" ]
 	then
 		die "Cannot find curl"
 	fi
 
-	if [ ! -x "/usr/bin/git" -o ! -x "$(which git)" ]
+	if [ ! -x "/usr/bin/git" -a ! -x "$(which git)" ]
 	then
 		die "Cannot find git"
 	fi
 
-	if [ ! -x "/usr/bin/php" -o ! -x "$(which php)" ]
+	if [ ! -x "/usr/bin/php" -a ! -x "$(which php)" ]
 	then
 		die "Cannot find php"
 	fi
 
-	if [ ! -x "/usr/bin/gpg" -o ! -x "$(which gpg)" ]
+	if [ ! -x "/usr/bin/gpg" -a ! -x "$(which gpg)" ]
 	then
 		die "Cannot find gpg"
 	fi
 
-	if [ ! -x "/usr/bin/zip" -o ! -x "$(which zip)" ]
+	if [ ! -x "/usr/bin/zip" -a ! -x "$(which zip)" ]
 	then
 		die "Cannot find zip"
 	fi
 
-	if [ ! -x "/usr/bin/mail" -o ! -x "$(which mail)" ]
+	if [ ! -x "/usr/bin/mail" -a ! -x "$(which mail)" ]
 	then
 		die "Cannot find mail"
 	fi
 
-	if [ ! -x "/usr/bin/git-lfs" -o ! -x "$(which git-lfs)" ]
+	if [ ! -x "/usr/bin/md5sum" -a ! -x "$(which md5sum)" ]
+	then
+		die "Cannot find md5sum"
+	fi
+
+	if [ ! -x "/usr/bin/git-lfs" -a ! -x "$(which git-lfs)" ]
 	then
 		echo "Warning: Cannot find git-lfs. Cloning Matomo may take more space than usual..."
 		echo "Warning: Hit CTRL+C to stop now, or wait a few seconds to continue."
@@ -280,7 +291,7 @@ function organizePackage() {
 		fi
 	fi
 
-	find ./ -type f -printf '%s ' -exec md5sum {} \; \
+	$FIND ./ -type f -printf '%s ' -exec md5sum {} \; \
 		| grep -v "user/.htaccess" \
 		| egrep -v 'manifest.inc.php|vendor/autoload.php|vendor/composer/autoload_real.php' \
 		| sed '1,$ s/\([0-9]*\) \([a-z0-9]*\) *\.\/\(.*\)/\t\t"\3" => array("\1", "\2"),/;' \
@@ -312,7 +323,7 @@ else
 	fi
 fi
 
-if [ -z "$3" ]; then
+if [ -z "$3" ] || [ "$3" == "false" ]; then
 	CACHE=false
 else
 	if [ "$3" == "true" ]; then
@@ -321,6 +332,17 @@ else
 	else
 		Usage "$0"
 	fi
+fi
+
+if [ -z "$4" ] || [ "$4" == "false" ]; then
+    BUILD_ONLY=false
+else
+    if [ "$4" == "true" ]; then
+        BUILD_ONLY=true
+        echo "Only building package"
+    else
+        Usage "$0"
+    fi
 fi
 
 # check for local requirements
@@ -354,7 +376,7 @@ for F in $FLAVOUR; do
 		#export GIT_TRACE_PACKET=1
 		#export GIT_TRACE=1
 		#export GIT_CURL_VERBOSE=1
-		git clone --config filter.lfs.smudge="git-lfs smudge --skip" "$URL_REPO" "$LOCAL_REPO"
+		git clone --config filter.lfs.smudge="git-lfs smudge --skip" --depth=1 --branch="$VERSION" "$URL_REPO" "$LOCAL_REPO"
 		if [ "$?" -ne "0" -o ! -d "$LOCAL_REPO" ]
 		then
 			die "Error: Failed to clone git repository $URL_REPO"
@@ -375,33 +397,18 @@ for F in $FLAVOUR; do
 	# we need to exclude LFS files from the upcoming git clone/git checkout,
 	# unfortunately this git config command does not work...
 	git config lfs.fetchexclude "tests/"
-	# ^^ not working, LFS files are fetched below... why?!
-
-	git checkout master --force
-	git reset --hard origin/master
-	git checkout master
-	git pull
-
-	# fetch everything
-	git fetch --tags --all --prune
-
-	echo "checkout repository for tag $VERSION..."
 
 	git branch -D "build" > /dev/null 2> /dev/null
 
-	echo -e "Commit UI tests git-lfs files to avoid some problems checking out the tag..."
-	git add plugins/*/tests/UI/ tests/UI/expected-screenshots/*
-	git commit -m'committing UI tests to avoid git checkout failures...'
-
-	echo -e "Now checking out the tag!"
-	git checkout -b "build" "tags/$VERSION" > /dev/null
+	echo -e "Creating branch for tag!"
+	git checkout -b "build" > /dev/null
 	[ "$?" -eq "0" ] || die "tag $VERSION does not exist in repository"
 
 	# clone submodules that should be in the release
 	for P in $(git submodule status | egrep $SUBMODULES_PACKAGED_WITH_CORE | awk '{print $2}')
 	do
 		echo -e "cloning submodule $P"
-		git submodule update --init $P
+		git submodule update --init --depth=1 $P
 	done
 
 	# leave $LOCAL_REPO folder
@@ -409,15 +416,21 @@ for F in $FLAVOUR; do
 
 	echo "copying files to a new directory..."
 	[ -d "$F" ] && rm -rf "$F"
-	cp -pdr "$LOCAL_REPO" "$F"
+	$CP -pdr "$LOCAL_REPO" "$F"
 	cd "$F"
 
-	[ "$(git describe --exact-match --tags HEAD)" = "$VERSION" ] || die "could not checkout to the tag for this version, make sure tag exists"
+    if [ "$BUILD_ONLY" != true ]; then
+    	[ "$(git describe --exact-match --tags HEAD)" = "$VERSION" ] || die "could not checkout to the tag for this version, make sure tag exists"
+    fi
 
 	echo "Preparing release $VERSION"
 	echo "Matomo version in core/Version.php: $(grep "'$VERSION'" core/Version.php)"
 
-	[ "$(grep "'$VERSION'" core/Version.php | wc -l)" = "1" ] || die "version $VERSION does not match core/Version.php";
+    if [ "$BUILD_ONLY" != true ]; then
+    	[ "$(grep "'$VERSION'" core/Version.php | wc -l)" = "1" ] || die "version $VERSION does not match core/Version.php";
+    else
+        echo "BUILD_ONLY=true, skipping Version.php check."
+	fi
 
 	echo "Organizing files and generating manifest file..."
 	organizePackage
@@ -428,17 +441,34 @@ for F in $FLAVOUR; do
 	echo "packaging release..."
 	rm "../$LOCAL_ARCH/$F-$VERSION.zip" 2> /dev/null
 	zip -9 -r "../$LOCAL_ARCH/$F-$VERSION.zip" "$F" How\ to\ install\ Matomo.html > /dev/null
-	gpg --armor --detach-sign "../$LOCAL_ARCH/$F-$VERSION.zip" || die "Failed to sign $F-$VERSION.zip"
+	if [ "$BUILD_ONLY" != true ]; then
+    	gpg --armor --detach-sign "../$LOCAL_ARCH/$F-$VERSION.zip" || die "Failed to sign $F-$VERSION.zip"
+    else
+        cp "../$LOCAL_ARCH/$F-$VERSION.zip" "$CURRENT_DIR"
+    fi
 
 	rm "../$LOCAL_ARCH/$F-$VERSION.tar.gz"  2> /dev/null
 	tar -czf "../$LOCAL_ARCH/$F-$VERSION.tar.gz" "$F" How\ to\ install\ Matomo.html
-	gpg --armor --detach-sign "../$LOCAL_ARCH/$F-$VERSION.tar.gz" || die "Failed to sign $F-$VERSION.tar.gz"
+	if [ "$BUILD_ONLY" != true ]; then
+    	gpg --armor --detach-sign "../$LOCAL_ARCH/$F-$VERSION.tar.gz" || die "Failed to sign $F-$VERSION.tar.gz"
+    else
+        cp "../$LOCAL_ARCH/$F-$VERSION.tar.gz" "$CURRENT_DIR"
+    fi
 
 	rm "../$LOCAL_ARCH/$F-$VERSION-WAG.zip"  2> /dev/null
 	zip -9 -r "../$LOCAL_ARCH/$F-$VERSION-WAG.zip" "$F" install.sql Manifest.xml parameters.xml > /dev/null 2> /dev/null
-	gpg --armor --detach-sign "../$LOCAL_ARCH/$F-$VERSION-WAG.zip" || die "Failed to sign $F-$VERSION-WAG.zip"
+	if [ "$BUILD_ONLY" != true ]; then
+    	gpg --armor --detach-sign "../$LOCAL_ARCH/$F-$VERSION-WAG.zip" || die "Failed to sign $F-$VERSION-WAG.zip"
+    else
+        cp "../$LOCAL_ARCH/$F-$VERSION-WAG.zip" "$CURRENT_DIR"
+    fi
 
 done
+
+if [ "$BUILD_ONLY" == true ]; then
+    echo "Skipping remote work since BUILD_ONLY=true...";
+    exit;
+fi
 
 # #### #### #### #### #### #
 # let's do the remote work #
@@ -512,7 +542,6 @@ do
 			echo $REMOTE_CMD_WWW
 			$REMOTE_CMD_WWW "echo $VERSION > $WWW_PATH/LATEST" || die "cannot deploy new version file on piwik@$REMOTE_SERVER"
 			$REMOTE_CMD_WWW "echo $SIZE > $WWW_PATH/LATEST_SIZE" || die "cannot deploy new archive size on piwik@$REMOTE_SERVER"
-
 
 			# only show this message when it's for 'matomo'
 			if [ "$F" == "matomo" ];
